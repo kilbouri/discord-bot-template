52af2391675152d5a294539d3e4a80d28374bd62
Isaac Kilbourne <isaac@kilbourne.ca>
2023-09-16 23:14:27 -0400

    Remove un-needed option in ping command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -3,15 +3,7 @@ import {CacheType, ChatInputCommandInteraction} from "discord.js";
 import {CommandType} from ".";
 
 const pingCommand: CommandType = {
-    data: new SlashCommandBuilder()
-        .setName("ping")
-        .setDescription("Ping. Pong?")
-        .addBooleanOption(
-            new SlashCommandBooleanOption()
-                .setName("detailed")
-                .setDescription("Show extended statistics")
-                .setRequired(false)
-        ),
+    data: new SlashCommandBuilder().setName("ping").setDescription("Ping. Pong?"),
     deferMode: "NO-DEFER",
     execute: async (intr: ChatInputCommandInteraction<CacheType>) => {
         return intr.reply("Pong!");
